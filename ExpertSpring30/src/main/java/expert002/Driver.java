2d1a47ffc567e35b3a9f967814113f02f19b26f5
hooong <tjrwns0529@gmail.com>
2019-12-13 17:56:11 +0900

    DI with spring study

--- ExpertSpring30/src/main/java/expert002/Driver.java
+++ ExpertSpring30/src/main/java/expert002/Driver.java
@@ -1,11 +1,12 @@
 package expert002;
 
-import org.springframework.context.Applicationcontext;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 
 public class Driver {
     public static void main(String[] args) {
-        ApplicationContext context = new ClassPathXmlApplicationContext("expert002/expert002.xml");
+        ApplicationContext context = new ClassPathXmlApplicationContext("expert002.xml");
 
         Car car = context.getBean("car", Car.class);
 
