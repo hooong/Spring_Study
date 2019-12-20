c7fd5b90c8f677c8902fa09d94060686922aeab4
hooong <tjrwns0529@gmail.com>
2019-12-20 16:50:45 +0900

    inflearn spring core tech study ( ~IoC Container #2)

---
+++ springcontext/src/main/java/me/hooong/springcontext/ApplicationConfig.java
@@ -0,0 +1,23 @@
+package me.hooong.springcontext;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackageClasses = DemoApplication.class)
+public class ApplicationConfig {
+
+
+//    @Bean
+//    public BookRepository bookRepository() {
+//        return new BookRepository();
+//    }
+//
+//    @Bean
+//    public BookService bookService() {
+//        BookService bookService = new BookService();
+////        bookService.setBookRepository(bookRepository());
+//        return bookService;
+//    }
+}
---
+++ springcontext/src/main/java/me/hooong/springcontext/BookRepository.java
@@ -0,0 +1,7 @@
+package me.hooong.springcontext;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class BookRepository {
+}
