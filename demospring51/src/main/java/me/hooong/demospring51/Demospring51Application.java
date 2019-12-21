04f93c9e73cf037b7d5d752a15e7e8e79108bf0b
hooong <tjrwns0529@gmail.com>
2019-12-21 17:57:18 +0900

    @Component study

--- demospring51/src/main/java/me/hooong/demospring51/Demospring51Application.java
+++ demospring51/src/main/java/me/hooong/demospring51/Demospring51Application.java
@@ -1,10 +1,16 @@
 package me.hooong.demospring51;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import out.MyService;
 
 @SpringBootApplication
 public class Demospring51Application {
+
+	// 컴포넌트 스캔 범위를 벗어남.
+//	@Autowired
+//	MyService myService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Demospring51Application.class, args);
