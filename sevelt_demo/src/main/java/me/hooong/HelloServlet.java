122d7405f35a620313943c626c898dca13c4317b
hooong <tjrwns0529@gmail.com>
2019-12-31 15:16:26 +0900

    test commit

--- sevelt_demo/src/main/java/me/hooong/HelloServlet.java
+++ sevelt_demo/src/main/java/me/hooong/HelloServlet.java
@@ -18,7 +18,7 @@ public class HelloServlet extends HttpServlet {
         resp.getWriter().println("<html>");
         resp.getWriter().println("<head>");
         resp.getWriter().println("<body>");
-        resp.getWriter().println("<h1>Hello servlet</h1>");
+        resp.getWriter().println("<h1>Hello servlet!!</h1>");
         resp.getWriter().println("</body>");
         resp.getWriter().println("</head>");
         resp.getWriter().println("</html>");
