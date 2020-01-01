b883d1f046b66de5a73b07fac406310cf8795f05
hooong <tjrwns0529@gmail.com>
2020-01-01 21:02:35 +0900

    web.xml이 없을때

---
+++ sevelt_demo/src/main/java/me/hooong/WebApplication.java
@@ -0,0 +1,25 @@
+package me.hooong;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+// web.xml이 없이 이걸 사용할 수 있음.
+public class WebApplication implements WebApplicationInitializer {
+
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
+        context.register(WebConfig.class);
+        context.refresh();
+
+        DispatcherServlet dispatcherServlet = new DispatcherServlet(context);
+        ServletRegistration.Dynamic app = servletContext.addServlet("app", dispatcherServlet);
+        app.addMapping("/app/*");
+
+    }
+}
